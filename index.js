c225917fed68042798f7776fe59b7e03d5a633d7
Charles Lowell <cowboyd@frontside.io>
2015-02-25 22:55:50 -0600

    misspelled image name

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'ember-cli-sample-image-data',
 
   included: function(app) {
-    app.import('vendor/assets/dey.jpg');
+    app.import('vendor/assets/bey.jpg');
     app.import('vendor/assets/dowager-countess.jpg');
     app.import('vendor/assets/flippin-sweet.jpg');
     app.import('vendor/assets/shake-it-off.jpg');
